f5f4e2670974e291f26a040f61e4f1b23106fd92
shokir <shokir@gmail.com>
2019-09-04 15:07:14 +0300

    Added validation test 3

--- models/User.js
+++ models/User.js
@@ -1,17 +1,39 @@
+const validator = require("validator")
+
 let User = function(data) {
     this.data = data
     this.errors = []
 }
 
 User.prototype.validate = function(){
+    // no empty fields accepted
     if (this.data.username == ""){
         this.errors.push("You must provide a username.")
     }
-    if (this.data.email == ""){
+    if (this.data.username != "" && !validator.isAlphanumeric(this.data.username)){
+        this.errors.push("Username can only contain letters and numbers")
+    }
+    if (!validator.isEmail(this.data.email)){
         this.errors.push("You must provide a valid email address.")
     }
     if (this.data.password == ""){
         this.errors.push("You must provide a password.")
+    }
+    
+     // password validation
+     if (this.data.password.length > 0 && this.data.password.length < 12){
+        this.errors.push("Password should be longer than 12 chars.")
+    }
+    if (this.data.password.length > 100){
+        this.errors.push("Password cannot exceed 100 chars")
+    }
+
+     // username validation
+    if (this.data.username.length > 0 && this.data.username.length < 3){
+        this.errors.push("Username should be at least 3 chars.")
+    }
+    if (this.data.username.length > 30){
+        this.errors.push("Username cannot exceed 30 chars")
     }
 }
 
