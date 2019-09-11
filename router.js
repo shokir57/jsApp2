0729b80aeaa41dff26e093dad740a4ca53ec3ea2
shokir <shokir@gmail.com>
2019-09-11 12:11:21 +0300

    Viewing a Post - Part 1

--- router.js
+++ router.js
@@ -12,6 +12,7 @@ router.post("/logout", userController.logout)
 // post related routes
 router.get("/create-post", userController.mustBeLoggedIn, postController.viewCreateScreen)
 router.post("/create-post", userController.mustBeLoggedIn, postController.create)
+router.get("/post/:id", postController.viewSingle) // "/:id" makes it flexible. The reason we excluded userController is bcs we wanna make post public like any Blog websites.
 
 
 module.exports = router
