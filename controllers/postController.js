2c89091ea6849b999c9ead26ee56b2605f187193
shokir <shokir@gmail.com>
2019-09-14 23:47:52 +0300

    Changed the viewEditScreen function

--- controllers/postController.js
+++ controllers/postController.js
@@ -6,7 +6,7 @@ exports.viewCreateScreen = function(req, res){
 
 exports.create = function(req, res){
     let post = new Post(req.body, req.session.user._id)
-    
+
     post.create().then(function(newId) {
         req.flash("success", "New post successfully created.")
         req.session.save(() => res.redirect(`/post/${newId}`))
@@ -27,18 +27,17 @@ exports.viewSingle = async function(req, res){
 } 
 
 exports.viewEditScreen = async function(req, res) {
-    try {
-        let post = await Post.findSingleById(req.params.id)
-        if (post.authorId == req.visitorId) {
-            res.render("edit-post", {post: post})
-        } else {
-            req.flash("errors", "You do not have permission to perform this action.")
-            req.session.save(() => res.redirect("/") )
-        }
-    } catch {
-        res.render("404")
+  try {
+    let post = await Post.findSingleById(req.params.id, req.visitorId)
+    if (post.isVisitorOwner) {
+      res.render("edit-post", {post: post})
+    } else {
+      req.flash("errors", "You do not have permission to perform that action.")
+      req.session.save(() => res.redirect("/"))
     }
-
+  } catch {
+    res.render("404")
+  }
 }
 
 exports.edit = function(req, res) {
