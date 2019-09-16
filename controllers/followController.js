a2c0f01dcce673b1e7a45f4e53445712bf7a23fa
shokir <shokir@gmail.com>
2019-09-16 17:11:03 +0300

    Saving a Follow Action Into Our Database

--- controllers/followController.js
+++ controllers/followController.js
@@ -4,7 +4,7 @@ exports.addFollow  = function(req, res) {
     let follow = new Follow(req.params.username, req.visitorId)
     follow.create().then(() => {
         req.flash("success", `Successfully followed ${req.params.username}`)
-        req.session.save(() => res.redirect(`profile/${req.params.username}`))
+        req.session.save(() => res.redirect(`/profile/${req.params.username}`))
     }).catch((errors) => {
         errors.forEach(error => {
             req.flash("errors", error)
