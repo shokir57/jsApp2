bdc954bbeea104bce7ae713db4fad2e0c6499054
shokir <shokir@gmail.com>
2019-09-17 14:05:55 +0300

    Following Screen (Part 1)

--- models/Follow.js
+++ models/Follow.js
@@ -94,4 +94,31 @@ Follow.getFollowersById = function(id) {
         }
     })
 }
+
+Follow.getFollowingById = function(id) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let followers = await followsCollection.aggregate([
+                {$match: {authorId: id}},
+                {$lookup: {from: "users", localField: "followedId", foreignField: "_id", as: "userDoc"}},
+                // spell out what should be displayed in the returned object
+                {$project: {  
+                    username: {$arrayElemAt: ["$userDoc.username", 0]},
+                    email: {$arrayElemAt: ["$userDoc.email", 0]}
+                }}
+            ]).toArray()
+
+            followers = followers.map(function(follower) {
+                // create a user
+                let user = new User(follower, true)
+                return {username: follower.username, avatar: user.avatar}  // 2 pieces of data to show on the front end of our website
+            })
+            resolve(followers)
+        } catch {
+            reject()
+        }
+    })
+}
+
+
 module.exports = Follow
