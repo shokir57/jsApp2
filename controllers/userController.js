60b92f0f6f6dfa8a240e19a185d70c3fbcf80e08
shokir <shokir@gmail.com>
2019-09-18 17:50:20 +0300

    Live Form Validation (Part 3)

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,14 @@ const User = require("../models/User")  // Dot-dot-slash is how you move one dir
 const Post = require("../models/Post") 
 const Follow = require("../models/Follow") 
 
+exports.doesUsernameExist = function (req, res) {
+    User.findByUsername(req.body.username).then(function () {
+        res.json(true)
+    }).catch(function () {
+        res.json(false)
+    })
+}
+
 exports.sharedProfileData = async function (req, res, next) {
     isVisitorsProfile = false
     let isFollowing = false
