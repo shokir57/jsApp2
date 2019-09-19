8a6864ca1ce9eb9d4dd2290160bc707b96ccc8eb
shokir <shokir@gmail.com>
2019-09-19 17:16:30 +0300

    Finishing API - deleting Posts and Understanding CORS

--- controllers/postController.js
+++ controllers/postController.js
@@ -89,6 +89,14 @@ exports.delete = function(req, res) {
     })
 }
 
+exports.apiDelete = function(req, res) {
+    Post.delete(req.params.id, req.apiUser._id).then(() => {
+        res.json("Success")
+    }).catch(() => {
+        res.json("You do not have permission to perform that action.")
+    })
+}
+
 exports.search = function(req, res) {
     Post.search(req.body.searchTerm).then(posts => {
         res.json(posts)
